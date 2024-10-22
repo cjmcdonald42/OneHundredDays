110b4cd4c88c5846651e3392f456da9c0c5cb686
Charles J McDonald <cjmcdonald42@gmail.com>
2024-10-22 13:48:38 -0400

    Day 01, 02

---
+++ day01.py
@@ -0,0 +1,14 @@
+'''
+Day 01
+Print Statements
+'''
+
+name = "Chuck"
+
+# The new hotness:
+print(f"Hello, {name}!")
+
+# The old ways...
+print("Hello, " + name + "!")
+print("Hello,",name,"!", sep=" ", end="\n")
+print("Hello, %s!" % name)
---
+++ day02.py
@@ -0,0 +1,11 @@
+'''
+Day 02
+Input
+'''
+
+name = input("What is your name? : ")
+food = input("What is your favorite food? : ")
+music = input("What is your favorite music? : ")
+location = input("Where do you live? : ")
+
+print(f"Hello, {name}! You like {food} and listen to {music}. \nYou live in {location}.")
