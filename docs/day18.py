7cb28f22a4ac4a8775672d6c0caa0cbe879a3595
cjmcdonald <cjmcdonald@woonsocketschools.com>
2024-10-22 09:15:50 -0400

    Add Day 18

---
+++ docs/day18.py
@@ -0,0 +1,27 @@
+'''
+Guess a random number between 1 and 100
+'''
+
+import random
+
+# Pick a random number
+number = random.randint(1, 100)
+print(number)   # Cheating...
+
+isPlaying = True
+while isPlaying is True:
+    isGuessing = True
+    while isGuessing is True:
+        guess = int(input("Guess a number between 1 and 100: "))
+        if guess == number:
+            print("Correct! You Win!!!")
+            isGuessing = False
+        elif guess > number:
+            print("Too high!")
+        elif guess < number:
+            print("Too low!")
+
+    # Repeat the game
+    again = input("Would you like to play again? (y/n): ")
+    if again == "n"
+        isPlaying = False
